602e0212a306b34aa9332acac37a81d7b5ef8fd5
Rachelle Dauphin <rachelledauphin@Rachelles-MacBook-Pro.local>
2017-11-21 17:17:44 -0500

    Add follow and unfollow buttons, follower and following count to user profile. Style profile.

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,6 +1,5 @@
 class FollowsController < ApplicationController
   before_action :confirm_logged_in, only: [:update, :destroy]
-
   def update
     @currentUser = current_user
     @follower = User.find_by_id(params[:id])
@@ -13,8 +12,16 @@ class FollowsController < ApplicationController
     end
     redirect_to('/profile')
   end
-
   def destroy
-
+    @currentUser = current_user
+    @follower = User.find_by_id(params[:id])
+    puts @follower
+    @currentUser.followees.delete(@follower)
+    if @currentUser.followees.include?(@follower)
+      flash[:error] = ["Something else happened"]
+    else
+      flash[:notice] = "You unfollowed #{@follower.full_name}"
+    end
+    redirect_to('/profile')
   end
 end
