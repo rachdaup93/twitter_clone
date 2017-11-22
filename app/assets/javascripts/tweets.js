49c5103e5666300f8a929a0c102b69f9c377c1c7
Rachelle Dauphin <rachelledauphin@Rachelles-MacBook-Pro.local>
2017-11-22 14:24:31 -0500

    Fixed bug: changed .ready to .on('turbolinks:load',..

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
     $("#new-tweets .tweet_description").focus(function(){
         $(this).css('height', '100px')
-        setInterval(function(){
+        x= setInterval(function(){
         wordCount = $("#new-tweets .tweet_description").val()
-        $('#new-tweets .word-count').html(240 - wordCount.length)
+         $('#new-tweets .word-count').html(240 - wordCount.length)
         }, 100)
     })
     $("#new-tweets .tweet_description").blur(function(){
+        clearInterval(x);
         setTimeout(function(){ $("#new-tweets .tweet_description").css('height', '35px') }, 1000)
     })
 })
