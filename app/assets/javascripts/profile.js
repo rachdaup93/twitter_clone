8ce1ed50c6bf535578e551f687d8249b90faeee6
Rachelle Dauphin <rachdaup.tech@gmail.com>
2017-11-27 14:42:49 -0500

    Style personal profile page and profile editor forms. Add front-end javascript to provide preview of new image upload.

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,10 @@
+function readURL(input, id) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            $(id).attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
