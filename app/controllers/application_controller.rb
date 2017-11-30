35b46f114d638d0f0cf7c679da5c3c40736ddf4b
Rachelle Dauphin <rachdaup.tech@gmail.com>
2017-11-30 09:13:54 -0500

    Remove redundancy.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,33 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_out_path_for(resource)
-    stored_location_for(resource) ||
-      if resource.is_a?(User)
-        puts "TESTTSSSS"
-        '/login'
-      else
-        puts "MORE TESTS"
-        super
-      end
-  end
-
-  def devise_error_messages!
-    return "" if resource.errors.empty?
-
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
-    html = <<-HTML
-    <div id="error_explanation">
-      <p>#{messages}</p>
-    </div>
-    HTML
-
-    html.html_safe
-  end
-
   protected
 
   def configure_permitted_parameters
